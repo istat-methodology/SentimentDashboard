e95f378f030060a1b7206e5a3eec56c88c0eb494
liminalfever <f.ortame@hotmail.it>
2024-11-25 14:16:51 +0000

    Data Loading Debug

--- main.py
+++ main.py
@@ -7,29 +7,29 @@ bits.set_header()
 bits.set_session_states()
 bits.load_data()
 bits.global_filters()
-bits.get_filtered_data()
+#bits.get_filtered_data()
 
 overview_tab, frequency_tab, debug = st.tabs(['Overview', 'Frequency Analysis', 'Debug'])
 
-with overview_tab:
-    col1, col2 = st.columns(2)
-    with col1:
-        plots.VolumePlot().add()
-    with col2:
-        plots.QuantitativePlot().add()
-
-with frequency_tab:
-    col1, col2 = st.columns(2)
-    with col1:
-        plots.QualitativePlot().add()
-    with col2:
-        plots.SentimentClassTS().add()
-    
-    col3, col4 = st.columns(2)
-    with col3:
-        plots.WordFrequencyPlot().add()
-    with col4:
-        plots.SentimentPie().add()
+#with overview_tab:
+#    col1, col2 = st.columns(2)
+#    with col1:
+#        plots.VolumePlot().add()
+#    with col2:
+#        plots.QuantitativePlot().add()
+#
+#with frequency_tab:
+#    col1, col2 = st.columns(2)
+#    with col1:
+#        plots.QualitativePlot().add()
+#    with col2:
+#        plots.SentimentClassTS().add()
+#    
+#    col3, col4 = st.columns(2)
+#    with col3:
+#        plots.WordFrequencyPlot().add()
+#    with col4:
+#        plots.SentimentPie().add()
 
 with debug:
     st.json(st.session_state['data'].data)
